3fe29eb592a5b4ad32025919a8604e6dae373fe3
Andrew Low <roo@lowtek.ca>
2023-04-07 11:27:10 -0400

    add RMLIMIT to control sync if we have many deletes

--- entrypoint.sh
+++ entrypoint.sh
@@ -39,11 +39,30 @@ if ! ssh $SSHOPT -i /config/private.key $ACCOUNT test -e ./external/MOUNTED; the
     echo "Remote external drive is not detected"
     exit
 fi
+#
+# Check how many files are going to be deleted
+#
+DELETED=$(rsync -avz --dry-run --delete --delete-excluded --stats -e "ssh $SSHOPT -i /config/private.key" /encrypted $ACCOUNT:./external | fgrep 'Number of deleted files' | cut -d' ' -f5 | tr -d ,)
 
+#
+# Only perform actual rsync if the deleted number is low enough, and it is not forced
+#
+if(($DELETED > $RMLIMIT)) && [[ ! -f /config/force ]]; then
+   echo -e "\n\nDetected "$DELETED" deletions, exceeds limit of "$RMLIMIT" deleted files, aborting."
+   echo -e "Create ./config/force to force backup to proceed\n\n"
+else
 # 
+# Announce and clear force if present
+#
+if [[ -f /config/force ]]; then
+  echo "Backup forced, removing ./config/force"
+  rm /config/force
+fi
+#
 # Peform rsync
 #
 timeout $TIMEOUT rsync -avz --bwlimit=$BWLIMIT --delete --delete-excluded --stats -e "ssh $SSHOPT -i /config/private.key" /encrypted $ACCOUNT:./external
+fi
 
 # Umount the crypted fs
 umount /encrypted
